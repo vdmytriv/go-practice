3ba1e0aac56c2f6ec2853b736319e8916a4d4767
vdmytriv <dmytrivv@gmail.com>
2020-03-06 15:38:16 +0200

    added leetcode sample

---
+++ leetcode/TwoSum/main.go
@@ -0,0 +1,14 @@
+package TwoSum
+
+func twoSum(nums []int, target int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
