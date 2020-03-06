ef4c15318b224cf57121f3a70b82dd6e29174d5e
vdmytriv <dmytrivv@gmail.com>
2020-03-06 18:41:00 +0200

    added hackerrank sample test

---
+++ hackerrank/ViralAdvertising/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the viralAdvertising function below.
+func viralAdvertising(n int32) int32 {
+	if n == 1 {
+		return 2
+	}
+
+	return viralAdvertising(n-1) + liked(n)
+}
+
+func shared(n int32) int32 {
+	if n == 1 {
+		return 5
+	}
+
+	return liked(n-1) * 3
+}
+
+func liked(n int32) int32 {
+	if n == 1 {
+		return 5 / 2
+	}
+
+	return shared(n) / 2
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	result := viralAdvertising(n)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ hackerrank/ViralAdvertising/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Test_viralAdvertising(t *testing.T) {
+	type args struct {
+		n int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{
+			"simple test",
+			args{5},
+			24,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := viralAdvertising(tt.args.n); got != tt.want {
+				t.Errorf("viralAdvertising() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
