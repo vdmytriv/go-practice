7f7f49f09849327f52650a46412c0d9ab999158d
vdmytriv <dmytrivv@gmail.com>
2020-03-09 11:09:23 +0200

    solved FindDigits without convertion to string

--- hackerrank/FindDigits/main.go
+++ hackerrank/FindDigits/main.go
@@ -17,3 +17,22 @@ func findDigits(n int32) int32 {
 
 	return c
 }
+
+func findDigits2(n int32) int32 {
+	c := int32(0)
+	rest := n
+	var last int32
+	for {
+		if rest <= 0 {
+			break
+		}
+
+		last, rest = rest%10, rest/10
+
+		if last != 0 && n%last == 0 {
+			c++
+		}
+	}
+
+	return c
+}
--- hackerrank/FindDigits/main_test.go
+++ hackerrank/FindDigits/main_test.go
@@ -1,6 +1,8 @@
 package FindDigits
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_findDigits(t *testing.T) {
 	type args struct {
@@ -21,6 +23,30 @@ func Test_findDigits(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := findDigits(tt.args.n); got != tt.want {
 				t.Errorf("findDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findDigits2(t *testing.T) {
+	type args struct {
+		n int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{
+			"simple test 2",
+			args{1012},
+			3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findDigits2(tt.args.n); got != tt.want {
+				t.Errorf("findDigits2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
